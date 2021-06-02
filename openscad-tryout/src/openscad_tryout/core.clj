20346a21dec57281cb0596c8accc8ec7c76a8067
runge <henrik@runge.biz>
2021-06-02 12:18:12 +0200

    add collumns
    A bit messy, but I am trying different aproaches

--- openscad-tryout/src/openscad_tryout/core.clj
+++ openscad-tryout/src/openscad_tryout/core.clj
@@ -5,52 +5,68 @@
 
 (def space 14.07)
 (def c-dist 19.05)
-(def kb-vec ["",""])
+(def kb-vec [["","",""],["","S"],["","",""]])
 
-(def width (* c-dist (count kb-vec)))
+(def height (* c-dist (count kb-vec)))
+(def width (* c-dist (apply max (map #(count %) kb-vec))))
 
-(def plater 
-  (union
-   (plate 1.4 width space 5)  
-   (plate 6.4 width (+ space 3 ) 0)
-   (extrude-linear {:height 10 :center false}
-                   (difference 
-                    (square (+ width 2) (+ c-dist 2))
-                    (square width  c-dist)))))
+
+(defn mount-holes [t]
+  (with-local-vars [main-hole-d (/ 4.2 2), connector-hole-d (/ 3.2 2), side-hole-d (/ 1.9 2)]    
+    (with-fn 120
+      (circle @main-hole-d)
+      (translate [-2.54 -5.08] (circle  @connector-hole-d))
+      (translate [3.81 -2.54] (circle @connector-hole-d))
+      (translate [-5 0] (circle @side-hole-d))
+      (translate [5 0] (circle @side-hole-d)))))
+
+(defn mount-rows [i row cut]
+  (translate [0 (* (* c-dist i) -1)]
+             (map-indexed #(translate [(* c-dist %1 ) 0] (cut %2)) row)))
 
 
 (def mount-plate 
-  (extrude-linear {:height 1.5}
-                  (difference 
-                   (translate [(/ c-dist -2) (/ c-dist -2)] 
-                              (square width c-dist :center false ))
-                   (map-indexed #(holes %1 %2) kb-vec))))
+  (extrude-linear {:height 1.5}  
+                  (difference
+                   (square width height )
+                   (translate  [(+ (/ width -2) (/ c-dist 2)) (- (/ height 2) (/ c-dist 2))]
+                               (map-indexed #(mount-rows %1 %2 mount-holes) kb-vec)))))
 
 
-(defn plate [h w s f]
-  (extrude-linear {:height h :center false}
+(def hole 
+  (square 12 12))
+
+(defn top-rows [i row method]
+  (translate [0 (* (* c-dist i) -1)]
+             (map-indexed #(translate [(* c-dist %1 ) 0] (method)) row)))
+
+
+(defn t-plate [t w h frame s]
+  (extrude-linear {:height t :center false}
                   (difference 
-                   (square (+ w f) (+ c-dist f) :center true )
-                   (translate [(- (/ w -2) (/ c-dist -2)) 0 ] 
+                   (square (+ w frame) (+ h frame))
+                   (translate  [(+ (/ w -2) (/ c-dist 2)) (- (/ h 2) (/ c-dist 2))] 
+                               (map-indexed #(top-rows %1 %1 hole) kb-vec)))))
+
+(defn plate [t w h s f]
+  (extrude-linear {:height t :center false}
+                  (difference 
+                   (square (+ w f) (+ h f) :center true )
+                   (translate [(- (/ w -2) (/ c-dist -2)) (- (/ h 2) (/ c-dist 2) ) ] 
                               (for [x (range (count kb-vec))]
-                                (translate [(* c-dist x) 0 ] 
-                                           (square s s :center true)))))))
+                                (translate [0 (* (* c-dist x) -1) ]
+                                           (for [y (range (count (get kb-vec x)))] 
+                                             (translate [ (* (* c-dist y) 1) 0]
+                                                        (square s s :center true)))))))))
 
-
-(defn holes [a b]
-  (with-local-vars [main-hole-d (/ 4.2 2), connector-hole-d (/ 3.2 2), side-hole-d (/ 1.9 2)]
-    (translate [(* c-dist a) 0 ]    
-               (with-fn 120
-                 (difference (circle @main-hole-d  )
-                             (text b :halign "center" :valign "center" :size 3))  
-                 (translate [-2.54 -5.08] (circle  @connector-hole-d))
-                 (translate [3.81 -2.54] (circle @connector-hole-d))
-                 (translate [-5 0] (circle @side-hole-d))
-                 (translate [5 0] (circle @side-hole-d))))))
-
-(def cap 
-  (full-cap 9 6 round-cap-form)
-  )
+(def top-plate
+  (union 
+   (plate 1.4 width height space 5)  
+   (plate 5.0 width height (+ space 3) 1)
+   (extrude-linear {:height 10 :center false}          
+                   (difference 
+                    (square (+ width 2) (+ height 2))
+                    (square width height)))))
 
 (defn full-cap [r h base-form]
   (with-fn 120
@@ -84,6 +100,13 @@
                      (translate [0 0 (* r 5)] 
                                 (sphere (* r 4.47)))))))
 
+(def cap 
+  (full-cap 9 6 round-cap-form)
+  )
 
-(spit "../../post-demo.scad"
-      (write-scad cap ))
+(spit "../scads/top-plate.scad"
+      (write-scad top-plate ))
+
+(spit "../scads/mount-plate.scad"
+       (write-scad mount-plate ))
+ 
