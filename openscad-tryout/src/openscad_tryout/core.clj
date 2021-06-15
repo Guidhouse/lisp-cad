3ee5b9519c3b6c811a7f9c9d0b88662bbcfbd750
runge <henrik@runge.biz>
2021-06-15 10:13:44 +0200

    add logic for the 'h' modifier

--- openscad-tryout/src/openscad_tryout/core.clj
+++ openscad-tryout/src/openscad_tryout/core.clj
@@ -10,7 +10,7 @@
 (def height (* c-dist (count kb-vec)))
 
 (defn row_width [v]
-  (+ (apply + (map  #(get % :x 0) v))
+  (+ (apply + (map #(get % :x 0) v))
      (count (filter #(= (type %) java.lang.String ) v))))
 
 (def width (* c-dist (apply max (map #(row_width %) kb-vec))))
@@ -24,16 +24,23 @@
       (model/translate [-5 0] (model/circle @side-hole-d))
       (model/translate [5 0] (model/circle @side-hole-d)))))
 
+(defn modified_hole [x y r cut]
+  (model/translate [x y] 
+                   (model/rotate [0 0 (/ model/pi r)] cut))) 
+
 
 (defn s-row [x row cut holes]
-  (def is_hole (= (type (first row)) java.lang.String))
-  (def x_mod (if is_hole (+ x c-dist) (get-x-mods (first row))))
+
+  (def x_mod  (+ x c-dist))
+  (def y_mod  (* (/ (get (first row) :h 0) -4) c-dist))
   (if (empty? row)
     holes
-    (if is_hole
-      (recur x_mod (rest row) cut (conj holes (model/translate [x_mod  0] cut)))      
-      (recur  (+ x  (*(get (first row) :x) c-dist)) (rest row) cut holes)
-      )))
+    (if (> (get (first row) :x 0) 0)
+        (recur (+ x  (* (get (first row) :x 0) c-dist)) (rest row) cut holes)
+    (if (> (get (first row) :h 0) 0)
+      (recur x_mod (rest (rest row)) cut (conj holes (modified_hole x_mod y_mod -2 cut)))
+      (recur x_mod (rest row) cut (conj holes (model/translate [x_mod  0] cut)))
+     ))))
 
 (defn rows [y row cut]
   (model/translate [0 (* (* c-dist y) -1)]
@@ -50,7 +57,7 @@
   (model/extrude-linear {:height 1.5}  
                         (model/difference
                          (model/square width height )
-                         (model/translate  [(+ (/ width -2) (/ c-dist 2)) (- (/ height 2) (/ c-dist 2))]
+                         (model/translate  [(- (/ width -2) (/ c-dist 2)) (- (/ height 2) (/ c-dist 2))]
                                            (map-indexed #(rows %1 %2 mount-holes) kb-vec)))))
 
 (defn top-hole [s]
