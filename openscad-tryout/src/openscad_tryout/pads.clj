d668e987af120a8ee7e0dccdc9ad0a25b15f2cea
runge <henrik@runge.biz>
2021-06-25 05:50:38 +0200

    add rounding top to pads

--- openscad-tryout/src/openscad_tryout/pads.clj
+++ openscad-tryout/src/openscad_tryout/pads.clj
@@ -10,21 +10,33 @@
 
 (defn base [dist rad h]
   (model/extrude-linear {:height h :center false}
-                        (model/with-fn 200
-                          (model/hull
-                           (model/translate [(/ dist 2) 0]
-                                            (model/circle rad))
-                           (model/translate [(/ dist -2) 0]
-                                            (model/circle rad))))))
+                        (model/hull
+                         (model/translate [(/ dist 2) 0]
+                                          (model/circle rad))
+                         (model/translate [(/ dist -2) 0]
+                                          (model/circle rad)))))
 
+(defn intersector [dist rad]
+  (with-local-vars [sf 2]
+    (model/hull
+     (model/translate [(/ dist 2) 0]
+                      (model/scale [1.8 1.8 1]
+                                   (model/sphere rad)))
+     (model/translate [(/ dist -2) 0]
+                      (model/scale [1.8 1.8 1]
+                                   (model/sphere rad))))))
 
 
 (def pad
-  (model/union
-   (model/difference
-    (base c-dist radius 1.8)
-    (base  c-dist 2.1 0.9))
-   (base  c-dist 1.3 0.9)))
+  (model/with-fn 150
+    (model/intersection
+     (model/union
+      (model/difference
+       (base c-dist radius 1.8)
+       (base  c-dist 2.1 0.9))
+      (base  c-dist 1.2 1))
+     (intersector c-dist 1.8))
+))
 
 
 (spit "../scads/pads.scad"
