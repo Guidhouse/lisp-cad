28125ef6afb7161a35b5bd20c00a377f54b77ba2
runge <henrik@runge.biz>
2021-07-01 08:56:43 +0200

    use let for readabillity

--- openscad-tryout/src/openscad_tryout/stand.clj
+++ openscad-tryout/src/openscad_tryout/stand.clj
@@ -3,17 +3,16 @@
             [scad-clj.scad :as scad]))
 
 (def support-angle (/ model/pi 8/3))
-(def laptop-thickness 17)
-(def base-length 120)
+(def laptop-thickness 12)
 (def support-length 80)
 (def front-support-length 20)
 (def base-thickness 8)
 (def support-thickness 4)
 (def arc-radius (* support-length 0.8))
-
 (def support-distance 
   "Distance of support rods calculated from thickness of laptop"
   (/ (+ laptop-thickness support-thickness) (Math/sin support-angle)))
+(def base-length (+ support-length (* support-distance 1.5)))
 
 (def support-offset 
   "Position the front support rod to give a smooth transistion"
@@ -41,30 +40,29 @@
 (defn smoother [f & block]
   "Smooths the corners by doing offset both ways."
   (model/offset (- 0 f)
-     (model/offset f block)))
+                (model/offset f block)))
 
 (def cutouts
   "Cutouts to give a more airy feel to the stand and save some plastic"
   (model/union
-   (model/circle (/ base-thickness 4))
-   (model/translate 
-    [(/ base-thickness 1.5) 0 0]
-    (base-rod (- support-distance (/ base-thickness 0.75) (- 0  (first support-offset)) ) 
-              (/ base-thickness 2.5)))
+   (model/circle (/ base-thickness 5))
+   (let [rod-length (- support-distance (/ base-thickness 0.75) (- 0  (first support-offset)) )]
+     (model/translate [(/ base-thickness 1.5) 0 0]
+      (base-rod rod-length (/ base-thickness 2.5))))
    (model/translate 
     [(- support-distance (- 0 (first support-offset))) (rest support-offset) 0]
-    (model/circle (/ base-thickness 5)))
-   (model/translate 
-    [(+ support-distance (/ base-thickness 1.5)  (first support-offset)) 0 0] 
-    (base-rod (- (* support-length 0.8) (/ base-thickness 0.75)) 
-              (/ base-thickness 2.5)))
-   (model/translate 
-    [(+ support-distance arc-radius (first support-offset)) (rest support-offset) 0] 
-    (model/circle (/ base-thickness 5)))
-   (model/translate 
-    [(+ support-distance arc-radius (/ base-thickness 1.5) (first support-offset) ) 0 0] 
-    (base-rod (- (- base-length base-thickness  (first support-offset)) support-distance arc-radius (/ base-thickness 1.5))
-              (/ base-thickness 2.5)))))
+    (model/circle (/ support-thickness 2)))
+   (let [start-x (+ support-distance (/ base-thickness 1.5)  (first support-offset))
+         rod-length  (- (* support-length 0.8) (/ base-thickness 0.75))]
+     (model/translate [start-x 0 0] 
+                      (base-rod rod-length (/ base-thickness 2.5))))
+   (let [start-x (+ support-distance arc-radius (first support-offset))]
+     (model/translate [start-x (rest support-offset) 0] 
+                      (model/circle (* (/ support-thickness 2) 0.8))))
+   (let [start-x (+ support-distance arc-radius (/ base-thickness 1.5) (first support-offset) )
+         rod-length (- (- base-length base-thickness  (first support-offset)) support-distance arc-radius (/ base-thickness 1.5))] 
+     (model/translate [start-x 0 0] 
+                      (base-rod rod-length (/ base-thickness 2.5))))))
 
 (def stand
   (model/extrude-linear
@@ -72,8 +70,8 @@
    (model/difference
     (model/union
      (model/translate support-offset 
-      (model/rotate [0 0 support-angle] 
-       (base-rod front-support-length support-thickness)))
+                      (model/rotate [0 0 support-angle] 
+                                    (base-rod front-support-length support-thickness)))
      (smoother 4
                (base-rod (- base-length base-thickness) base-thickness)
                (model/translate support-offset 
