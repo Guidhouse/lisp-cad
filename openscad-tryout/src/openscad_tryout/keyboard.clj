1a08d4e18b221f6ad709ad7276c5df58083d4fa0
runge <henrik@runge.biz>
2021-07-06 23:37:18 +0200

    add padding

---
+++ openscad-tryout/src/openscad_tryout/keyboard.clj
@@ -0,0 +1,76 @@
+(ns openscad-tryout.keyboard
+  (:require [clojure.data.json :as json]
+            [scad-clj.model :as model]
+            [scad-clj.scad :as scad]))
+
+(def space 14.07)
+(def c-dist 19.05)
+(def kb-vec (json/read-json (slurp "input.json")))
+
+(def height (* c-dist (count kb-vec)))
+
+(defn row_width [v]
+  (+ (apply + (map #(get % :x 0) v))
+     (count (filter #(= (type %) java.lang.String ) v))))
+
+(def width (* c-dist (apply max (map #(row_width %) kb-vec))))
+
+(def mount-holes
+  (with-local-vars [main-hole-d (/ 4.2 2), connector-hole-d (/ 3.2 2), side-hole-d (/ 1.9 2)]    
+    (model/with-fn 120
+      (model/circle @main-hole-d)
+      (model/translate [-2.54 -5.08] (model/circle  @connector-hole-d))
+      (model/translate [3.81 -2.54] (model/circle @connector-hole-d))
+      (model/translate [-5 0] (model/circle @side-hole-d))
+      (model/translate [5 0] (model/circle @side-hole-d)))))
+
+(defn top-hole [x y]
+  (model/square x y))
+
+(defn modified_hole [x y r cut]
+  (model/translate [x y] 
+                   (model/rotate [0 0 (/ model/pi r)] cut))) 
+
+
+(defn s-row [x row cut holes]
+  (def x_mod  (+ x c-dist))
+  (def y_mod  (* (/ (get (first row) :h 0) -4) c-dist))
+  (if (empty? row)
+    holes
+    (if (> (get (first row) :x 0) 0)
+        (recur (+ x  (* (get (first row) :x 0) c-dist)) (rest row) cut holes)
+    (if (> (get (first row) :h 0) 0)
+      (recur x_mod (rest (rest row)) cut (conj holes (modified_hole x_mod y_mod -2 cut)))
+      (recur x_mod (rest row) cut (conj holes (model/translate [x_mod  0] cut)))
+     ))))
+
+(defn rows [y row cut]
+  (model/translate [0 (* (* c-dist y) -1)]
+                   (s-row 0 row cut ())))
+
+(defn t-plate [t w h s f]
+  (model/extrude-linear {:height t :center false}  
+                        (model/difference
+                         (model/square (+ w f) (+ h f) :center true )
+                         (model/translate  [(- (/ w -2) (/ c-dist 2)) (- (/ h 2) (/ c-dist 2))]
+                                           (map-indexed #(rows %1 %2 (top-hole s s)) kb-vec)))))
+
+(def mount-plate
+  (model/extrude-linear {:height 1.5}  
+                        (model/difference
+                         (model/square width height )
+                         (model/translate  [(- (/ width -2) (/ c-dist 2)) (- (/ height 2) (/ c-dist 2))]
+                                           (map-indexed #(rows %1 %2 mount-holes) kb-vec)))))
+
+
+(def top-plate
+  (model/union 
+   (t-plate 1.4 width height space 5 )  
+   (t-plate 5.0 width height (+ space 3) 1)
+   (model/extrude-linear {:height 10 :center false}          
+                         (model/difference 
+                          (model/square (+ width 2) (+ height 2))
+                          (model/square width height)))))
+
+
+
--- openscad-tryout/src/openscad_tryout/stand.clj
+++ openscad-tryout/src/openscad_tryout/stand.clj
@@ -3,12 +3,12 @@
             [scad-clj.scad :as scad]))
 
 (def support-angle (/ model/pi 8/3))
-(def laptop-thickness 20.5)
-(def support-length 80)
+(def laptop-thickness 11)
+(def support-length 90)
 (def front-support-length 20)
 (def base-thickness 10)
 (def support-thickness 4)
-(def arc-radius (* support-length 0.9))
+(def arc-radius (* support-length 0.95))
 (def support-distance 
   "Distance of support rods calculated from thickness of laptop"
   (/ (+ laptop-thickness support-thickness) (Math/sin support-angle)))
@@ -46,8 +46,8 @@
   "Cutouts to give a more airy feel to the stand and save some plastic"
   (model/union
    (model/circle (/ base-thickness 5))
-   (model/translate [(/ base-thickness 3) (- 0 (/ base-thickness 2) -1) 0]
-                    (model/circle 1.75))
+   (model/translate [(/ base-thickness 3) (- 0 (/ base-thickness 2) -0.2) 0]
+                    (model/circle (/ 1.75 2)))
    (let [rod-length (- support-distance (/ base-thickness 0.75) (- 0  (first support-offset)) )]
      (model/translate [(/ base-thickness 1.5) 0 0]
       (base-rod rod-length (/ base-thickness 2.5))))
@@ -61,10 +61,10 @@
    (let [start-x (+ support-distance arc-radius (first support-offset))]
      (model/translate [start-x (nth support-offset 1) 0] 
                       (model/circle (* (/ support-thickness 2) 0.8))
-     (model/translate [0 (- 0 (/ base-thickness 2)  (nth support-offset 1)  -1) 0]
-                   (model/circle 1.75))))
+                      (model/translate [0 (- 0 (/ base-thickness 2) (nth support-offset 1) -0.2) 0]
+                                       (model/circle (/ 1.75 2)))))
    (let [start-x (+ support-distance arc-radius (/ base-thickness 1.5) (first support-offset) )
-         rod-length (- (- base-length base-thickness  (first support-offset)) support-distance arc-radius (/ base-thickness 1))] 
+         rod-length (- (- base-length base-thickness  (first support-offset)) support-distance arc-radius (/ base-thickness 1.5))] 
      (model/translate [start-x 0 0] 
                       (base-rod rod-length (/ base-thickness 2.5))))
 ))
